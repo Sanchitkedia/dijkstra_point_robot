250e5a20d5bba32a47b0148cec8fe824dff0dd20
Sanchit kedia <sanchit@terpmail.umd.edu>
2023-03-09 11:27:42 -0500

    Create new function to append node; Update ClosedList dict

--- dijkstra_sanchit_kedia.py
+++ dijkstra_sanchit_kedia.py
@@ -6,11 +6,11 @@ import heapq as hq
 
 def create_cv_map(base_map):
     # Draw obstacles
-    base_map = cv2.rectangle(base_map,(100,0),(150,100),(0,0,255),-1)
-    base_map = cv2.rectangle(base_map,(100,150),(150,250),(0,0,255),-1)
-    base_map = cv2.fillPoly(base_map,[np.array([[460,25],[460,225],[510,125]], np.int32)],(0,0,255))
-    base_map = cv2.fillPoly(base_map,[np.array([[300,50],[235,87],[235,162],[300,200],[365,163],[365,88]], np.int32)],(0,0,255))
-    obstacle_map = cv2.rectangle(base_map,(0,0),(600,250),(0,0,255),5)
+    base_map = cv2.rectangle(base_map,(100,0),(150,100),(0,255,255),-1)
+    base_map = cv2.rectangle(base_map,(100,150),(150,250),(0,255,255),-1)
+    base_map = cv2.fillPoly(base_map,[np.array([[460,25],[460,225],[510,125]], np.int32)],(0,255,255))
+    base_map = cv2.fillPoly(base_map,[np.array([[300,50],[235,87],[235,162],[300,200],[365,163],[365,88]], np.int32)],(0,255,255))
+    obstacle_map = cv2.rectangle(base_map,(0,0),(600,250),(0,255,255),5)
 
 
     return obstacle_map
@@ -45,7 +45,7 @@ def create_cv_map(base_map):
 #                 pygame.quit()
 #                 exit()
 
-def user_input(obstacle_map):
+def UserInput(obstacle_map):
 
     start = []
     goal = []
@@ -66,7 +66,7 @@ def user_input(obstacle_map):
         print("The start point is inside an obstacle. Please enter a valid start point.")
 
     start.append(start_x)
-    start.append(start_y)
+    start.append(250-start_y)
     
     while True:
         goal_x = int(input("Enter the x coordinate of the goal point: "))
@@ -84,7 +84,7 @@ def user_input(obstacle_map):
         print("The goal point is inside an obstacle. Please enter a valid goal point.")
     
     goal.append(goal_x)
-    goal.append(goal_y)
+    goal.append(250-goal_y)
 
     # obstacle_map = cv2.circle(obstacle_map, (start_x, start_y), 1, (255,255,255), -1)
     # obstacle_map = cv2.circle(obstacle_map, (goal_x, goal_y), 1, (255,255,255), -1)
@@ -95,7 +95,7 @@ def ActionMoveUp(node, obstacle_map):
     new_node = []
     new_node.append(node[0])
     new_node.append(node[1] + 1)
-    if (new_node[1] <= 250) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[1] <= 250) and (obstacle_map[obstacle_map.shape[0]- new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -104,7 +104,7 @@ def ActionMoveDown(node, obstacle_map):
     new_node = []
     new_node.append(node[0])
     new_node.append(node[1] - 1)
-    if (new_node[1] >= 0) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[1] >= 0) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -113,7 +113,7 @@ def ActionMoveLeft(node, obstacle_map):
     new_node = []
     new_node.append(node[0] - 1)
     new_node.append(node[1])
-    if (new_node[0] >= 0) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[0] >= 0) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -122,7 +122,7 @@ def ActionMoveRight(node, obstacle_map):
     new_node = []
     new_node.append(node[0] + 1)
     new_node.append(node[1])
-    if (new_node[0] < 600) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[0] < 600) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -131,7 +131,7 @@ def ActionMoveUpLeft(node, obstacle_map):
     new_node = []
     new_node.append(node[0] - 1)
     new_node.append(node[1] + 1)
-    if (new_node[0] >= 0) and (new_node[1] < 250) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[0] >= 0) and (new_node[1] < 250) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -140,7 +140,7 @@ def ActionMoveUpRight(node, obstacle_map):
     new_node = []
     new_node.append(node[0] + 1)
     new_node.append(node[1] + 1)
-    if (new_node[0] < 600) and (new_node[1] < 250) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[0] < 600) and (new_node[1] < 250) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -149,7 +149,7 @@ def ActionMoveDownLeft(node, obstacle_map):
     new_node = []
     new_node.append(node[0] - 1)
     new_node.append(node[1] - 1)
-    if (new_node[0] >= 0) and (new_node[1] >= 0) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[0] >= 0) and (new_node[1] >= 0) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
@@ -158,165 +158,111 @@ def ActionMoveDownRight(node, obstacle_map):
     new_node = []
     new_node.append(node[0] + 1)
     new_node.append(node[1] - 1)
-    if (new_node[0] < 600) and (new_node[1] >= 0) and (obstacle_map[obstacle_map.shape[0]-1 - new_node[1]][new_node[0]][0] == 1):
+    if (new_node[0] < 600) and (new_node[1] >= 0) and (obstacle_map[obstacle_map.shape[0] - new_node[1]][new_node[0]][0] == 1):
         return new_node
     else:
         return None
 
-def CheckGoal(node, goal):
+def CheckGoal(node, goal,start, obstacle_map,ClosedList,start_time):
     if node[0] == goal[0] and node[1] == goal[1]:
-        return True
+        print("\nGoal Reached")
+        print("\nPath Length: ", len(ClosedList))
+        end_time = time.time()
+        print("\nTime taken: ", end_time - start_time, "seconds")
+        obstacle_map = Backtrack(start, goal, ClosedList, obstacle_map)
     else:
         return False
 
-def backtrack(start, goal, ClosedList, obstacle_map):
+def CheckNode(node_new,ClosedList,OpenList,current_cost,current_node,cost):
+    node_new = tuple(node_new)
+    if node_new not in ClosedList:
+        found = True
+        for node in OpenList:
+            if node[2] == node_new:
+                idx = OpenList.index(node)
+                found = False
+                if node[0] > current_cost + cost:
+                    OpenList[idx][0] = current_cost + cost
+                    OpenList[idx][1] = current_node[2]
+                break
+        if found:
+            hq.heappush(OpenList, [current_cost + cost, current_node[2], node_new])
+
+def Backtrack(start, goal, ClosedList, obstacle_map):
     path = []
     path.append(goal)
     current_node = goal
+    for key in list(ClosedList.keys()):
+        obstacle_map[key[1], key[0]] = (255,255,255)
+        cv2.imshow("Exploration", obstacle_map)
+        cv2.waitKey(1)
     while current_node != start:
         current_node = ClosedList[(current_node[0],current_node[1])][1]
         path.append(current_node)
     path.reverse()
-    print(path)
     for i in range(len(path)):
-        obstacle_map[path[i][1], path[i][0]] = (0,0,255)
-    cv2.imshow("Path", obstacle_map)
+        obstacle_map[path[i][1], path[i][0]] = (255,0,0)
+
+    obstacle_map[start[1], start[0]] = (0,255,0)
+    obstacle_map[goal[1], goal[0]] = (0,255,0)
+
+    # show the image in the same window 
+    cv2.imshow("Exploration", obstacle_map)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def DijkstraPlanner(start, goal, obstacle_map):
+
+    OpenList = []
+    ClosedList = {}
+    node_start = [0.0, start, start]
+    hq.heappush(OpenList, node_start)
+    hq.heapify(OpenList)
+    start_time = time.time()
+    
+    while (len(OpenList) > 0):
+        current_node = hq.heappop(OpenList)
+        ClosedList[(current_node[2][0],current_node[2][1])] =  current_node[0], current_node[1]
+        current_cost = current_node[0]
+        CheckGoal(current_node[2], goal, start, obstacle_map, ClosedList,start_time)
+        
+        new_node = ActionMoveUp(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.0)
+        
+        new_node = ActionMoveDown(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.0)
+
+        new_node = ActionMoveLeft(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.0)
+
+        new_node = ActionMoveRight(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.0)
+
+        new_node = ActionMoveUpLeft(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.4)
+
+        new_node = ActionMoveUpRight(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.4)
+
+        new_node = ActionMoveDownLeft(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.4)
+
+        new_node = ActionMoveDownRight(current_node[2],obstacle_map)
+        if new_node is not None:
+            CheckNode(new_node,ClosedList,OpenList,current_cost,current_node,1.4)
 
 def main():
     obstacle_map = np.ones((250,600,3), dtype=np.uint8)
     obstacle_map = create_cv_map(obstacle_map)
-    start, goal = user_input(obstacle_map)
-    # print(start, goal)
-
-    OpenList = []
-    ClosedList = {}
-    node1 = (0, start, start)
-    hq.heappush(OpenList, node1)
-    hq.heapify(OpenList)
-
-    # closed_list[(node[2][0],node[2][1])] = node[1]
-    
-    while len(OpenList) > 0:
-        current_node = hq.heappop(OpenList)
-        ClosedList[(current_node[2][0],current_node[2][1])] =  current_node[0], current_node[1]
-        current_cost = np.float32(current_node[0])
-        if CheckGoal(current_node[2], goal):
-            print("Goal reached!")
-            obstacle_map = backtrack(start, goal, ClosedList, obstacle_map)
-            break
-        new_node = ActionMoveUp(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-       
-        new_node = ActionMoveDown(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-
-        new_node = ActionMoveLeft(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-
-        new_node = ActionMoveRight(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-
-        new_node = ActionMoveUpLeft(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1.4:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-
-        new_node = ActionMoveUpRight(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1.4:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-
-        new_node = ActionMoveDownLeft(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1.4:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
-
-        new_node = ActionMoveDownRight(current_node[2],obstacle_map)
-        if new_node is not None:
-            new_node = tuple(new_node)
-            if new_node not in ClosedList:
-                new_node = list(new_node)
-                hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-            else:
-                for node in ClosedList:
-                    if node == new_node:
-                        if ClosedList[node][0] > current_cost + 1.4:
-                            hq.heappush(OpenList, (current_cost + 1, current_node[2], new_node))
-                            ClosedList.remove(node)
-                            break
+    start, goal = UserInput(obstacle_map)
+    DijkstraPlanner(start, goal, obstacle_map)
 
 if __name__ == '__main__':
     main()
